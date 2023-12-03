b9957ab190712c573738d589bf9630c297d939a6
Junior Tiemele <akaaugustejunior@gmail.com>
2023-12-03 13:33:59 -0500

    add test account module

---
+++ src/entities/account.ts
@@ -0,0 +1,16 @@
+class Account {
+    constructor(title: string, credit_value: number) {
+    }
+
+    get_title(): string {
+        return '';
+    }
+
+    get_credit_value(): number {
+        return 0;
+    }
+}
+
+export {
+    Account
+}
---
+++ src/tests/entities/account.test.ts
@@ -0,0 +1,13 @@
+import { Account } from '../../entities/Account';
+
+describe('Test Account module', () => {
+    test('Creation Account', () => {
+        let credit_value = 1000;
+        let title = 'Perso';
+
+        let account = new Account(title, credit_value);
+
+        expect(account.get_title()).toBe(title);
+        expect(account.get_credit_value()).toBe(credit_value)
+    })
+})
