c96c8bd9cbbe8ec2631642226e1b592febbd3fca
Junior Tiemele <akaaugustejunior@gmail.com>
2024-01-22 15:56:04 -0500

    add tag router

---
+++ src/controllers/native/tag/router.ts
@@ -0,0 +1,63 @@
+import { Request, Response, Router } from 'express';
+import { categoryRepository, tagRepository } from '../resources/initInMemoryRepository';
+import { CreationTagUseCase } from '../../../interactions/tag/creationTagUseCase';
+import { ValidationError } from '../../../interactions/errors/validationError';
+import { GetTagUseCase } from '../../../interactions/tag/getTagUseCase';
+import { GetAllTagUseCase } from '../../../interactions/tag/getAllTagsUseCase';
+import { DeleteTagUseCase } from '../../../interactions/tag/deleteTagUseCase';
+
+const router = Router();
+
+router.post('/', (req: Request, res: Response) => {
+    try {
+        // data mapper
+        
+        if (req.body.title != undefined) {
+            throw new ValidationError('Title field is empty');
+        }
+        let titleTag = req.body.title;
+
+        let usecase = new CreationTagUseCase(tagRepository);
+
+        let response = usecase.execute(titleTag);
+
+        res.status(201).send(response);
+    } catch(error:any) {
+        res.status(400).send(error.message);
+    }
+});
+
+router.get('/:title', (req: Request, res: Response) => {
+    try {
+        let usecase = new GetTagUseCase(categoryRepository);
+        
+        let response = usecase.execute(req.params.title);
+
+        res.status(201).send(response);
+    } catch (error: any) {
+        res.status(400).send(error.message);
+    }
+});
+
+router.get('/', (req: Request, res: Response) => { 
+    try {
+        let usecase = new GetAllTagUseCase(categoryRepository);
+        let response = usecase.execute();
+        res.status(201).send(response);   
+    } catch (error: any) {
+        res.status(400).send(error.message);
+    }
+});
+
+
+router.delete('/:title', (req: Request, res: Response) => {
+    try {
+        let usecase = new DeleteTagUseCase(categoryRepository);
+        let response = usecase.execute(req.params.title);
+        res.status(201).send(response);   
+    } catch (error: any) {
+        res.status(400).send(error.message);
+    }
+});
+
+export default router;
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
 import AccountRouter from '../src/controllers/native/accounts/router';
 import TransactionRouter from '../src/controllers/native/transactions/router';
+import CategoryRouter from '../src/controllers/native/categories/router';
+import TagRouter from '../src/controllers/native/tag/router';
 
 dotenv.config();
 
@@ -14,6 +16,8 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 app.use('/account', AccountRouter);
 app.use('/transaction', TransactionRouter);
+app.use('/category', CategoryRouter);
+app.use('/tag', TagRouter);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + Typescript ');
