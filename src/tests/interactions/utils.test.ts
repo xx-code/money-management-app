bbf7cfc267c1b99d0a849afb9f300138e141ca6e
Junior Tiemele <akaaugustejunior@gmail.com>
2024-01-22 21:16:32 -0500

    add formatted fonction

--- src/tests/interactions/utils.test.ts
+++ src/tests/interactions/utils.test.ts
@@ -1,3 +1,4 @@
+import { formatted, reverseFormatted } from '../../interactions/utils/formatted';
 import { is_empty } from '../../interactions/utils/verify_empty_value';
 
 test('Verify empty value', () => {
@@ -6,4 +7,30 @@ test('Verify empty value', () => {
 
     result = is_empty('dffd');
     expect(result).toBe(false);
+});
+
+describe('Test formatted category value', () => {
+    it('Formatted test', () => {
+        let value = ' cateGory ';
+        let true_value = 'CATEGORY';
+
+        let result = formatted(value);
+        expect(result).toBe(true_value);
+
+        value = 'CATEGORY';
+        true_value = 'Category'
+
+        result = reverseFormatted(value);
+        expect(result).toBe(true_value);
+
+        value = 'Tag vaLue example';
+        true_value = 'TAG_VALUE_EXAMPLE';
+        result = formatted(value);
+        expect(result).toBe(true_value);
+
+        value = 'TAG_VALUE_EXAMPLE';
+        true_value = 'Tag value example';
+        result = reverseFormatted(value);
+        expect(result).toBe(true_value);
+    });
 });
