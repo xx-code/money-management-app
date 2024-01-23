9cd8eb85e443452ea7d9f6fdd90cdf69f6e9739a
Junior Tiemele <akaaugustejunior@gmail.com>
2024-01-23 01:30:47 -0500

    corrige bug category and tag repository

--- src/infrastructure/inMemory/inMemoryCategoryRepository.ts
+++ src/infrastructure/inMemory/inMemoryCategoryRepository.ts
@@ -2,39 +2,31 @@ import { dbCategory } from "../../interactions/repositories/categoryRepository";
 import { TagRepository, dbTag } from "../../interactions/repositories/tagRepository";
 
 export class InMemoryCategoryRepository implements TagRepository {
-    private db: Array<dbCategory> = [];
+    private db: Map<string, dbCategory> = new Map();
 
     get(title: string): dbCategory | null {
-        console.log(this.db)
-        let categories = this.db.filter((cat) => cat.title == title);
-        console.log('value')
-        console.log(categories)
+        let category = this.db.get(title);
 
-        if (categories.length <= 0) {
+        if (category == undefined) {
             return null;
         }
 
-        return categories[0];
+        return category;
     }
 
     get_all(): dbCategory[] {
-        return this.db;
+        return Array.from(this.db.values());
     }
 
     save(category: dbCategory): string {
-        this.db.push({title: category.title, icon: category.icon});
+        this.db.set(category.title, {title: category.title, icon: category.icon});
 
         return category.title;
     }
 
     delete(title: string): boolean {
-        let categories = this.db.filter((cat) => cat.title != title);
+        let resposne = this.db.delete(title);
 
-        if (categories.length >= this.db.length) {
-            return false;
-        }
-        this.db = categories;
-        
-        return true;
+        return resposne;
     }
 }
--- src/infrastructure/inMemory/inMemoryTagRepository.ts
+++ src/infrastructure/inMemory/inMemoryTagRepository.ts
@@ -1,36 +1,31 @@
 import { TagRepository, dbTag } from "../../interactions/repositories/tagRepository";
 
 export class InMemoryTagRepository implements TagRepository {
-    private db: Array<dbTag> = [];
+    private db: Map<string, dbTag> = new Map();
 
     get(title: string): dbTag | null {
-        let tags = this.db.filter((tag) => tag.title == title);
+        let tag = this.db.get(title);
 
-        if (tags.length <= 0) {
+        if (tag == undefined) {
             return null;
         }
 
-        return tags[0];
+        return tag;
     }
 
     get_all(): dbTag[] {
-        return this.db;
+        return Array.from(this.db.values());
     }
 
     save(tag: dbTag): string {
-        this.db.push({title: tag.title});
+        this.db.set(tag.title, {title: tag.title});
 
         return tag.title;
     }
 
     delete(title: string): boolean {
-        let tags = this.db.filter((tag) => tag.title != title);
+        let response = this.db.delete(title);
 
-        if (tags.length >= this.db.length) {
-            return false;
-        }
-        this.db = tags;
-
-        return true;
+        return response;
     }
 }
