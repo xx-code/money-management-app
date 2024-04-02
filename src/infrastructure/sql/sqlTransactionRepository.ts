1ad38c26a678ebb5c09a99451bcf08b2ec3f3af7
Junior Tiemele <akaaugustejunior@gmail.com>
2024-04-02 15:40:11 -0400

    add get transaction on sql repository

--- src/infrastructure/sql/sqlTransactionRepository.ts
+++ src/infrastructure/sql/sqlTransactionRepository.ts
@@ -1,11 +1,16 @@
 import { Account } from "@/core/entities/account";
 import { TransactionRepository, dbTransaction, dbTransactionPaginationResponse, dbFilter, dbSortBy } from "../../core/interactions/repositories/transactionRepository";
-import { Transaction } from "@/core/entities/transaction";
+import { Transaction, Record } from "@/core/entities/transaction";
+import { Category } from "@/core/entities/category";
 
 export class SqlTransactionRepository implements TransactionRepository {
     private db: any;
     private table_name: string;
     private is_table_exist: boolean = false;
+    private table_account_name: string = '';
+    private table_category_name: string = '';
+    private table_record_name: string = '';
+    private table_tag_name: string = '';
 
     constructor(db: any, table_name: string) {
         this.db = db;
@@ -17,13 +22,11 @@ export class SqlTransactionRepository implements TransactionRepository {
             CREATE TABLE IF NOT EXISTS ${this.table_name} (
                 id TEXT PRIMARY KEY,
                 id_account TEXT NOT NULL,
-                id_tag_list TEXT,
                 id_category TEXT NOT NULL,
                 id_record TEXT NOT NULL,
-                FOREIGN KEY (id_account) REFERENCES ${table_account_name},
-                FOREIGN KEY (id_category) REFERENCES ${table_category_name},
-                FOREIGN KEY (id_record) REFERENCES ${table_record_name},
-                FOREIGN KEY (id_tag_list) REFERENCES ${this.table_name}_tags
+                FOREIGN KEY (id_account) REFERENCES ${table_account_name}(id),
+                FOREIGN KEY (id_category) REFERENCES ${table_category_name}(title),
+                FOREIGN KEY (id_record) REFERENCES ${table_record_name}(id)
             )
         `);
 
@@ -31,10 +34,16 @@ export class SqlTransactionRepository implements TransactionRepository {
             CREATE TABLE IF NOT EXISTS ${this.table_name}_tags (
                 id_transaction TEXT NOT NULL,
                 id_tag TEXT NOT NULL,
-                FOREIGN KEY (id_transaction) REFERENCES ${this.table_name},
-                FOREIGN KEY (id_tag) REFERENCES ${table_tag_name}
+                FOREIGN KEY (id_transaction) REFERENCES ${this.table_name}(id)
+                    ON DELETE CASCADE
+                FOREIGN KEY (id_tag) REFERENCES ${table_tag_name}(title)  
             )
         `);
+        
+        this.table_account_name = table_account_name;
+        this.table_category_name = table_category_name;
+        this.table_record_name = table_record_name;
+        this.table_tag_name = table_tag_name;
 
         this.is_table_exist = true;
     }
@@ -46,28 +55,103 @@ export class SqlTransactionRepository implements TransactionRepository {
             }
 
             let result = await this.db.run(`
-                INSERT INTO ${this.table_name} (id, id_account, id_tag_list, id_category, id_record) VALUES (?, ?, ?, ?, ?)`,
-                request.id, request.account_ref, null, request.category_ref, request.record_ref,
+                INSERT INTO ${this.table_name} (id, id_account, id_category, id_record) VALUES (?, ?, ?, ?)`,
+                request.id, request.account_ref, request.category_ref, request.record_ref,
             );
 
+            let saved_tag = true;
             if (request.tag_ref.length > 0) {
-                for (let tag in request.tag_ref) {
-                    await this.db.run(`
+                for (let tag of request.tag_ref) {
+                    let result = await this.db.run(`
                         INSERT INTO ${this.table_name}_tags (id_transaction, id_tag) VALUES (?, ?)`,
                         request.id, tag
                     );
+                    saved_tag = result != undefined;
                 }
             }
 
-            if (result != undefined ) {
+            if (result != undefined && saved_tag) {
                 resolve(true);
             } else {
+                // TODO: Delete row when saving tags fault;
                 resolve(false);
             } 
         });
     }
     get(id: string): Promise<Transaction | null> {
-        throw new Error("Method not implemented.");
+        return new Promise(async (resolve, reject) => {
+            if (!this.is_table_exist) {
+                throw Error("Table transaction not created");
+            }
+            
+            
+            let result = await this.db.get(`
+                SELECT 
+                    ${this.table_name}.id, 
+                    ${this.table_account_name}.id as account_id,  ${this.table_account_name}.title as account_title, ${this.table_account_name}.credit_value, ${this.table_account_name}.credit_limit, 
+                    ${this.table_record_name}.id  as record_id, ${this.table_record_name}.price, ${this.table_record_name}.date, ${this.table_record_name}.description, ${this.table_record_name}.type,
+                    ${this.table_category_name}.title  as category_title, ${this.table_category_name}.icon
+                FROM 
+                    ${this.table_name} 
+                JOIN ${this.table_account_name}
+                    ON ${this.table_account_name}.id = ${this.table_name}.id_account
+                JOIN ${this.table_record_name}
+                    ON ${this.table_record_name}.id = ${this.table_name}.id_record
+                JOIN ${this.table_category_name}
+                    ON ${this.table_category_name}.title = ${this.table_name}.id_category
+                WHERE ${this.table_name}.id = ?`,
+                id
+            );
+
+            if(result != undefined) {
+                let result_tag = await this.db.all(`
+                    SELECT * 
+                    FROM 
+                        ${this.table_name}_tags
+                    JOIN ${this.table_tag_name}
+                        ON ${this.table_tag_name}.title = ${this.table_name}_tags.id_tag
+                    WHERE ${this.table_name}_tags.id_transaction = ?
+                    `,
+                    id
+                );
+
+                let tags = []
+                for (let tag of result_tag) {
+                    tags.push(tag['title']);
+                }
+
+                let account: Account = {
+                    id: result['account_id'],
+                    title: result['account_title'],
+                    credit_limit: result['credit_limit'],
+                    credit_value: result['credit_value'],
+                }
+
+                let category: Category = {
+                    title: result['category_title'],
+                    icon: result['icon']
+                }
+
+                let record: Record = {
+                    id: result['record_id'],
+                    price: result['price'],
+                    date: new Date(result['date']),
+                    description: result['description'],
+                    type: result['type']
+                }
+
+                resolve({
+                    id: id,
+                    account: account,
+                    record: record,
+                    category: category,
+                    tags: tags
+                });
+            
+            } else {
+                resolve(null);
+            }
+        });
     }
     get_transactions_by_categories(category_ref: string[], start_date: Date, end_date: Date): Promise<Transaction[]> {
         throw new Error("Method not implemented.");
--- src/tests/infrastructure/sql/sqlTransactionRepository.test.ts
+++ src/tests/infrastructure/sql/sqlTransactionRepository.test.ts
@@ -89,4 +89,144 @@ describe('Test transaction sql repository', () => {
 
         expect(is_saved).toBe(true);
     });
+
+    test('Test create add new transaction with tag', async () => {
+        let account_repo = new SqlAccountRepository(db, 'accounts');
+        await account_repo.create_table();
+
+        let new_account: Account = {
+            id: '1',
+            title: 'title',
+            credit_limit: 1250,
+            credit_value: 6600
+        };
+        await account_repo.save(new_account);
+
+        let category_repo = new SqlCategoryRepository(db, 'categories');
+        await category_repo.create_table();
+        let new_category: Category = {
+            title: 'cat',
+            icon: 'ico-cat'
+        }
+        await category_repo.save(new_category);
+
+        let tag_repo = new SqlTagRepository(db, 'tags');
+        await tag_repo.create_table();
+        let new_tag: Tag = 'tag';
+        await tag_repo.save({title: new_tag});
+
+        let record_repo = new SqlRecordRepository(db, 'records');
+        await record_repo.create_table();
+
+        let new_record: Record = {
+            id: 'record_1',
+            date: new Date(),
+            description: 'un blabla',
+            price: 15,
+            type: 'Credit'
+        };
+
+        await record_repo.save(new_record);
+
+        let transaction_repo = new SqlTransactionRepository(db, table_name);
+        await transaction_repo.create_table('accounts', 'categories', 'tags', 'records');
+
+        let new_transaction: dbTransaction = {
+            id: '1',
+            account_ref: '1',
+            tag_ref: [new_tag],
+            category_ref: 'cat',
+            record_ref: 'record_1'
+        }
+        let is_saved = await transaction_repo.save(new_transaction);
+
+        expect(is_saved).toBe(true);
+    });
+
+    test('Get  transaction', async () => {
+        let account_repo = new SqlAccountRepository(db, 'accounts');
+        await account_repo.create_table();
+
+        let new_account: Account = {
+            id: '1',
+            title: 'title',
+            credit_limit: 1250,
+            credit_value: 6600
+        };
+        await account_repo.save(new_account);
+
+        let category_repo = new SqlCategoryRepository(db, 'categories');
+        await category_repo.create_table();
+        let new_category: Category = {
+            title: 'cat',
+            icon: 'ico-cat'
+        }
+        await category_repo.save(new_category);
+
+        let tag_repo = new SqlTagRepository(db, 'tags');
+        await tag_repo.create_table();
+        let new_tag: Tag = 'tag';
+        await tag_repo.save({title: new_tag});
+
+        let record_repo = new SqlRecordRepository(db, 'records');
+        await record_repo.create_table();
+
+        let new_record: Record = {
+            id: 'record_1',
+            date: new Date(),
+            description: 'un blabla',
+            price: 15,
+            type: 'Credit'
+        };
+
+        await record_repo.save(new_record);
+
+        let new_record_with_tag: Record = {
+            id: 'record_2',
+            date: new Date(),
+            description: 'un blabla 2',
+            price: 18,
+            type: 'Credit'
+        };
+
+        await record_repo.save(new_record_with_tag);
+
+        let transaction_repo = new SqlTransactionRepository(db, table_name);
+        await transaction_repo.create_table('accounts', 'categories', 'tags', 'records');
+
+        let new_transaction: dbTransaction = {
+            id: '1',
+            account_ref: '1',
+            tag_ref: [],
+            category_ref: 'cat',
+            record_ref: 'record_1'
+        }
+        await transaction_repo.save(new_transaction);
+
+        let new_transaction_with_tag: dbTransaction = {
+            id: '2',
+            account_ref: '1',
+            tag_ref: [new_tag],
+            category_ref: 'cat',
+            record_ref: 'record_2'
+        }
+        await transaction_repo.save(new_transaction_with_tag);
+
+
+        let transaction = await transaction_repo.get('1');
+
+        expect(transaction).not.toBeNull();
+        expect(transaction?.account.id).toBe('1');
+        expect(transaction?.category.title).toBe('cat');
+        expect(transaction?.record.price).toBe(15);
+
+        let transaction_with_tag = await transaction_repo.get('2');
+
+        expect(transaction_with_tag).not.toBeNull();
+        expect(transaction_with_tag?.account.id).toBe('1');
+        expect(transaction_with_tag?.category.title).toBe('cat');
+        expect(transaction_with_tag?.record.price).toBe(18);
+        expect(transaction_with_tag?.tags.length).toBe(1);
+        expect(transaction_with_tag?.tags[0]).toBe('tag');
+    });
 });
